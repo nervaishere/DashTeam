8afde7a340b1626f5b25c0ea06fe79d78740c508
minerva <75211435+nervaishere@users.noreply.github.com>
2021-08-24 22:26:42 +0430

    Delete Python/4th_session directory

--- Python/4th_session/comparison.py
+++
@@ -1,9 +0,0 @@
-a=int(input('enter a number: '))
-b=int(input('enter a number: '))
-
-if a >=b:
-    max=a
-else:
-    max=b
-
-print(max)
--- Python/4th_session/indexing.py
+++
@@ -1,8 +0,0 @@
-text = 'Python'
-print(text[0])
-print(text[1])
-print(text[2])
-print(text[3])
-print(text[4])
-print(text[5])
-
