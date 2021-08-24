e44d90ce3a5081a1fd04bfa2a0dc835aa84a8100
nervaishere <nervaishere@gihub.com>
2021-08-24 18:07:43 +0430

    Add 4th session codes

---
+++ Python/4th_session/commenting.py
@@ -0,0 +1,17 @@
+
+""" Use * for multiplication of a number dash team python learning
+"""
+print(3 + 7)
+
+# Use * for multiplication of a number
+# The * is a multiplication operator in Python
+# To print the value, use the print() function
+print(3 * 7)
+
+"""
+Use * for multiplication of a number
+The * is a multiplication operator in Python
+To print the value, use the print() function
+"""
+
+print(4 * 7)
