a4581d7a21e95a1a2483f0c641aeae524eea3eef
nervaishere <athenajafari34@gmail.com>
2021-10-10 00:22:22 +0330

    Add 2.py

---
+++ python/HOMEWORK/9th_Session/Answers/Correct/2.py
@@ -0,0 +1,8 @@
+name = ""
+lastname = ""
+age = 0
+for i in range(10):
+    name , lastname , age = map(str,input("Enter user's information: ").split())
+    age = int(age)
+    if age>=18:
+        print(name,lastname,age)
